6e6d2897862d321d64e2960e904954f467f9642e
anac98 <amitnacson@gmail.com>
2019-11-15 02:55:47 -0600

    Getting started page, part1

--- starterGuide.php
+++ starterGuide.php
@@ -2,7 +2,38 @@
 <?php include('header.php');?>
 
 <div class="PageBody">
+<div class = 'cEPageBody'> 
+    <center>
+    <h1>Getting Started</h1>
+    </center>
+    <center>
+    <h2>What do I need to get started?</h2>
+    </center>
+    <div class = 'cryptoEx'>
+        <p>To start investing in Bitcoin and other cryptocurrencies you first need to sign up to an exchange which will allow you to buy cryptocurrency with cash. An exchange is basically an online platform that enables anyone to buy and sell Bitcoin as well as any other cryptocurrency that they have listed.
 
+        You may have to verify your account to buy or withdraw larger amounts of Bitcoin (or any other cryptocurrency) with most reputable exchanges.
+        Once you have bought your Bitcoin (or any other chosen cryptocurrency) you can either keep it on the exchange or have it transferred to your own personal wallet if you have one. You can always keep your coins on an exchange for the mean time whilst looking for a wallet.</p>
+    </div>
+    <center>
+    <h2>How do I keep track of the market?</h2>
+    </center>
+    <div class = 'cryptoEx'>
+    <p>Once you have bought your first Bitcoin and snagged yourself some “blue chip” cryptocurrencies (cryptocurrencies with a market capitalisation of over $2 Billion), you can start researching your chosen cryptocurrencies or research new ones.
+
+    One good place to start is Bitcointalk, this is a forum where cryptocurrency developers and investors can get together and discuss a particular cryptocurrency.
+
+    If you want to find a specific thread about a cryptocurrency, just type the cryptocurrencies name and the abbreviation “ANN” (short for announcement) into the search bar.</p>
+    </div>
+    <center>
+    <h2>Wallets</h2>
+    </center>
+    <div class = 'cryptoEx'>
+    <p>Bitcoin and other cryptocurrencies are “stored” using wallets, a wallet signifies that you own the cryptocurrency that was sent to the wallet. Every wallet has a public address and a private key.
+
+    You use the public address to recieve funds and the private key to spend the funds.</p>
+    </div>
+</div>
 </div>
 
 <?php include('footer.php');?>
